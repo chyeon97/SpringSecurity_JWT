15583138b53bfef562185f26f97a06a948a42672
chaeyeon <pcyeon07@gmail.com>
2022-05-29 00:41:30 +0900

    Style: 시큐리티 설정 주석 제거

--- src/main/java/com/example/springsecruity_jwt/config/SecurityConfig.java
+++ src/main/java/com/example/springsecruity_jwt/config/SecurityConfig.java
@@ -14,11 +14,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-//        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//            .and()
-//            .formLogin().disable()
-//            .httpBasic().disable()
-           http.authorizeRequests()
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .formLogin().disable()
+            .httpBasic().disable()
+            .authorizeRequests()
             .antMatchers("/api/user/**").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
             .antMatchers("/api/admin/**").access("hasRole('ROLE_ADMIN')")
             .anyRequest().permitAll();
