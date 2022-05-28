de5ffbef97ef5f544b10eccf6e871f66be2be903
chaeyeon <pcyeon07@gmail.com>
2022-05-28 17:51:57 +0900

    init Commit

---
+++ src/main/java/com/example/springsecruity_jwt/SpringSecruityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecruity_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecruityJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecruityJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsecruity_jwt/SpringSecruityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecruity_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecruityJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
